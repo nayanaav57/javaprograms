e5770d5ab16e87993edd2d8a3cee7ce6c1eb6065
nayanaav57 <nayanaav57@gmail.com>
2024-10-12 20:16:37 +0530

    committing factorial program

---
+++ FactorialNumber.java
@@ -0,0 +1,22 @@
+class FactorialNumber
+{
+public int calculate(int n)
+{
+int s=1;
+for (int i=n;i>0;i--)
+{
+s=s*i;
+}
+return (s);
+}
+public void print(int n)
+{
+int a=this.calculate(n);
+System.out.println("Factorial of "+ n +" is "+ a);
+}
+public static void main(String args[])
+{
+FactorialNumber obj=new FactorialNumber();
+obj.print(5);
+}
+}
